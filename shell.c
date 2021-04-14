26f8d5e6b0034fe6c5128fcbb49e67ec69065cbd
Thegermanblob <thegermanblob@gmail.com>
2021-04-14 23:36:48 +0000

    Betty comp

--- shell.c
+++ shell.c
@@ -11,14 +11,17 @@ int main(void)
 	pid_t PID;
 	int status;
 
+
 	while (1)
 	{
 		PID = fork();
+
 		if (PID == 0)
 		{
 			in = input();
 			av = get_argv(in);
 			status = execve(av[0], av, environ);
+
 			if (status == -1)
 			{
 				freearr(av);
@@ -26,11 +29,13 @@ int main(void)
 			}
 			else
 				freearr(av);
+
 		}
 		else if (PID == -1)
 			exit(EXIT_FAILURE);
 		else
 			wait(NULL);
+
 	}
 	return (0);
 }
