27798d20f482e427acf955af339d93a08d46b5e0
Your Name <iso.mora.cor@gmail.com>
2021-04-14 21:16:57 +0000

    fix

--- head.h
+++ head.h
@@ -1,22 +1,57 @@
-	
-#ifndef HEAD1
-#define HEAD1
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#ifndef HEADER_H
+#define HEADER_H
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#define _GNU_SOURCE
 
-char **inarr();
-void init_shell();
-int input(char **instr);
-void print_dir();
-void exec();
-void help();
-char *_strstr(char *haystack, char *needle);
+/**
+ * struct list_s - singly linked list
+ * @str: string tokenized of command line arguments.
+ * @next: points to the next node in the linked list.
+ *
+ * Description: singly linked list, the nodes are agregated at the end.
+ */
+typedef struct list_s
+{
+	char *str;
+	struct list_s *next;
+} list_t;
+
+/**
+ * struct list_env - singly linked list
+ * @key: string tokenized of command line arguments.
+ * @value: string tokenized of command line arguments.
+ *         Both arguments correspondig to 'KEY=value'.
+ * @next: points to the next node in the linked list.
+ *
+ * Description: singly linked list, the nodes are agregated at the end.
+ */
+typedef struct list_env
+{
+	char *key;
+	char *value;
+	struct list_env *next;
+
+} env_t;
+extern char **environ;
+char *get_commands(void);
+list_t *add_node_at_end(list_t **head, const char *str);
+int _strlen(const char *s);
+int _strcmp(const char *s1, const char *s2);
+char *_strdup(const char *str);
 char *_strcpy(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-char *_strdup(char *str);
-int execute(char **instr);
-
+char *_strcat(char *dest, char *src);
+char **get_argv(char *input);
+env_t *add_node_list_env(env_t **head, char *str);
+char *_getenv(const char *name);
+char **dir_path(char *cmd);
+void free_list(list_t *head);
+void free_list_env(env_t *head);
+void free_array(char **arr);
+void exe_builtin(char **vector);
 #endif
